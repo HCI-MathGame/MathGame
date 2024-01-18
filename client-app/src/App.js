b64cf848e2dd7bb033a2d415a1fbc84493a2aa8c
MarcusPaul16 <marcuspaul20021@gmail.com>
2024-01-19 01:09:32 +0200

    Prepare for deployment

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, Routes } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './Components/HomePage';
 import Level from './Components/Level';
 import NextLevelPage from './Components/NextLeveLConfirmation';
@@ -39,7 +39,7 @@ function App() {
     <div>
       <Router>
         <Routes>
-          <Route path='/MathGame' element={<HomePage />} />
+          <Route path='/' element={<HomePage />} />
           <Route path="/Level/:level" element={<Level />} />
           <Route path="/next-level/:level" element={<NextLevelPage />} />
           <Route path="/final" element={<CongratulationsPage />} />
