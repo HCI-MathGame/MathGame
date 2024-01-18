c3e562d9d7a178bc151bec444a4a0c27cabad156
MarcusPaul16 <marcuspaul20021@gmail.com>
2024-01-19 01:41:23 +0200

    Prepare for deployment

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -5,35 +5,8 @@ import HomePage from './Components/HomePage';
 import Level from './Components/Level';
 import NextLevelPage from './Components/NextLeveLConfirmation';
 import CongratulationsPage from './Components/FinalPage';
-import { useRef, useEffect, useState } from 'react';
 
 function App() {
-
-  const [userInteracted, setUserInteracted] = useState(false);
-  const audioRef = useRef(null);
-
-  useEffect(() => {
-    // Add an event listener for the first user interaction
-    const handleFirstUserInteraction = () => {
-      setUserInteracted(true);
-      window.removeEventListener('click', handleFirstUserInteraction);
-    };
-
-    window.addEventListener('click', handleFirstUserInteraction);
-
-    return () => {
-      window.removeEventListener('click', handleFirstUserInteraction);
-    };
-  }, []);
-
-  useEffect(() => {
-    // Play the audio only after the user has interacted
-    if (userInteracted) {
-      const audio = audioRef.current;
-      audio.volume = 0.3;
-      audio.play();
-    }
-  }, [userInteracted]);
 
   return (
     <div>
@@ -45,7 +18,6 @@ function App() {
           <Route path="/final" element={<CongratulationsPage />} />
         </Routes>
       </Router>
-      <audio ref={audioRef} src="/music.mp3" loop />
     </div>
   );
 }
